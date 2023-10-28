de758a58dde28237f175a99c034b63bf4e5d62df
MrEnessHS <76776068+MrEnessHS@users.noreply.github.com>
2023-10-28 19:19:07 +0300

    Update service.sh

--- service.sh
+++ service.sh
@@ -15,8 +15,10 @@ wait_boot_complete
 if [ -x "\$(command -v setenforce)" ]
 then
 	setenforce 0
+        chmod 700 /sys/fs/selinux/enforce
 else
 	echo -n 0 > /sys/fs/selinux/enforce
+        chmod 700 /sys/fs/selinux/enforce
 fi
 
 EOF
