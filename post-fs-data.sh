74a294d5b917331f5a977af2cfce3df5c2e96026
MrEnessHS <76776068+MrEnessHS@users.noreply.github.com>
2023-10-28 19:18:11 +0300

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,6 +1,8 @@
 if [ -x "$(command -v setenforce)" ]
 then
 	setenforce 0
+        chmod 700 /sys/fs/selinux/enforce
 else
 	echo -n 0 > /sys/fs/selinux/enforce
+        chmod 700 /sys/fs/selinux/enforce
 fi
